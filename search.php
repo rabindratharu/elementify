f6d636c451473efdad1a723cfb0989f07ab4c91f
rabindratharu <rabindra.tharu.np@gmail.com>
2024-11-13 07:16:03 +0545

    fixes search page

--- search.php
+++ search.php
@@ -7,8 +7,6 @@
  * @package Elementify
  */
 
-namespace Elementify;
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -20,71 +18,71 @@ get_header();
 /**
  * Functions hooked into elementify/before_content action
  *
+ * @hooked elementify_site_content_start    - 10
  */
 do_action( 'elementify/before_content' );
 ?>
 
-<main id="main" class="site-main">
+<?php
+/**
+ * Functions hooked into elementify/content_top action
+ * 
+ * @hooked elementify_posts_wrapper_start - 10
+ */
+do_action( 'elementify/content_top' );
+?>
 
-    <?php
+<?php if ( have_posts() ) : ?>
+
+<?php
 	/**
-	 * Functions hooked into elementify/before_content action
-	 *
+	 * Functions hooked into elementify/content/before_loop action
+	 * 
+	 * @hooked elementify_posts_page_header - 10
 	 */
-	do_action( 'elementify/content_top' );
-	?>
+	do_action('elementify/content/before_loop');
 
-    <?php if ( have_posts() ) : ?>
+	/* Start the Loop */
+	while ( have_posts() ) : the_post();
 
-    <?php
-		/**
-		 * Functions hooked into elementify/content/before_loop action
-		 * 
-		 * @hooked elementify_posts_page_header - 10
-		 */
-		do_action('elementify/content/before_loop');
+		/*
+		* Include the Post-Type-specific template for the content.
+		* If you want to override this in a child theme, then include a file
+		* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+		*/
+		get_template_part( 'template-parts/content', 'search' );
 
-		/* Start the Loop */
-		while ( have_posts() ) : the_post();
+	endwhile;
 
-			/*
-				* Include the Post-Type-specific template for the content.
-				* If you want to override this in a child theme, then include a file
-				* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				*/
-			get_template_part( 'template-parts/content', get_post_type() );
-
-		endwhile;
-
-		/**
-		 * Functions hooked into elementify/content/after_loop action
-		 *
-		 * @hooked elementify_posts_pagination - 10
-		 */
-		do_action('elementify/content/after_loop');
-
-	else :
-
-		get_template_part( 'template-parts/content', 'none' );
-
-	endif;
-	?>
-
-    <?php
 	/**
-	 * Functions hooked into elementify/content_bottom action
+	 * Functions hooked into elementify/content/after_loop action
 	 *
+	 * @hooked elementify_posts_wrapper_end	- 10
+	 * @hooked elementify_posts_pagination 	- 15
 	 */
-	do_action( 'elementify/content_bottom' );
-	?>
+	do_action('elementify/content/after_loop');
 
-</main><!-- #main -->
+else :
+
+	get_template_part( 'template-parts/content', 'none' );
+
+endif;
+?>
+
+<?php
+/**
+ * Functions hooked into elementify/content_bottom action
+ * 
+ * @hooked elementify_posts_wrapper_end - 10
+ */
+do_action( 'elementify/content_bottom' );
+?>
 
 <?php
 /**
  * Functions hooked into elementify/after_content action
  *
- * @hooked elementify_sidebar    - 10
+ * @hooked elementify_site_content_end	- 10
  */
 do_action( 'elementify/after_content' );
 ?>
