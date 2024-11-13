9825e054a789c9c35393c07924d2d1187f6ec107
rabindratharu <rabindra.tharu.np@gmail.com>
2024-11-13 07:16:45 +0545

    fixes

--- search.php
+++ search.php
@@ -50,6 +50,7 @@ do_action( 'elementify/content_top' );
 		* If you want to override this in a child theme, then include a file
 		* called content-___.php (where ___ is the Post Type name) and that will be used instead.
 		*/
+		//get_template_part( 'template-parts/content', get_post_type() );
 		get_template_part( 'template-parts/content', 'search' );
 
 	endwhile;
